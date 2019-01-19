5ca2b3d3a426d144cd165df28ef41fe36b6a7e66
Neeraj <nirajj58@gmail.com>
2019-01-19 12:26:47 -0500

    - Demo Model and Model attributes - Change View to use model attribute
    Signed-off-by: Neeraj Jadhav <nirajj58@gmail.com>

--- src/main/java/com/niraj/config/WebConfig.java
+++ src/main/java/com/niraj/config/WebConfig.java
@@ -1,11 +1,27 @@
 package com.niraj.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.niraj")
 public class WebConfig {
+
+    private static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    private static final String RESOLVER_SUFFIX = ".jsp";
+
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver urlBasedViewResolver = new InternalResourceViewResolver();
+        urlBasedViewResolver.setPrefix(RESOLVER_PREFIX);
+        urlBasedViewResolver.setSuffix(RESOLVER_SUFFIX);
+
+        return urlBasedViewResolver;
+    }
 }
--- src/main/java/com/niraj/controller/DemoController.java
+++ src/main/java/com/niraj/controller/DemoController.java
@@ -1,16 +1,39 @@
 package com.niraj.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
     // http://localhost:8080/todo-list/hello
+
     @ResponseBody
     @GetMapping("/hello")
     public String Hello() {
-        return "hello";
+        return "Hello! <a href='/todo-list/welcome'>Welcome<a>";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("user", "Niraj");
+        log.info("model= {}", model);
+
+        /* prefix + name +suffix
+         * /WEB-INF/view/welcome.jsp
+         * */
+        return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo application";
     }
 }
